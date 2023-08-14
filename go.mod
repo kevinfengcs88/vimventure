0502d974a9c20fd7067499b33d483a7f54a1d989
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-08-13 20:39:24 -0400

    Changed Vim benchmarking to time queried from NTP server

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/beevik/ntp v1.3.0 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
@@ -19,8 +20,9 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/term v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,60 +1,97 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"time"
 
-	"github.com/charmbracelet/bubbles/stopwatch"
+	"github.com/beevik/ntp"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+const NTP_SERVER = "129.6.15.28"
+
 type editorFinishedMsg struct{ err error }
 
-func openEditor(filename string, m model) tea.Cmd {
-	m.stopwatch.Start()
+func openEditor(filename string, start time.Time) tea.Cmd {
 	editor := os.Getenv("EDITOR")
 	if editor == "" {
 		editor = "vim"
 	}
 	c := exec.Command(editor, filename) //nolint:gosec
 	return tea.ExecProcess(c, func(err error) tea.Msg {
-		// calculateScore(filename)
-		fmt.Println(m.stopwatch.Elapsed())
+		stop, err := ntp.Time(NTP_SERVER)
+		if err != nil {
+			log.Fatal(err)
+		}
+		d := stop.Sub(start)
+		score := calculateScore("test.txt", d)
+		fmt.Println(score)
+		fmt.Println(score)
+		fmt.Println(score)
+		fmt.Println("WOOOOOOOOOOOOOOOOOOOOOOOOOOO")
 		return editorFinishedMsg{err}
 	})
 }
 
-func calculateScore(filename string) int {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+func scaleDuration(d time.Duration) float64 {
+	const (
+		threshold        = 60 * time.Second
+		maxValue         = 1.0
+		minValue         = 0.0
+		interval         = 5 * time.Second
+		scalePerInterval = (maxValue - minValue) / float64(threshold/interval)
+	)
 
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+	if d < interval {
+		return maxValue
 	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+	if d > threshold {
+		return minValue
 	}
-	return 3
+
+	intervalsPassed := float64(d / interval)
+	scaledValue := maxValue - (intervalsPassed * scalePerInterval)
+
+	return scaledValue
+}
+
+func calculateScore(filename string, d time.Duration) int {
+	// file, err := os.Open(filename)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
+	//
+	// scanner := bufio.NewScanner(file)
+	//
+	// for scanner.Scan() {
+	// 	line := scanner.Text()
+	// 	fmt.Println(line)
+	// }
+	//
+	// if err := scanner.Err(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// let's assume the file is modified correctly, and the player gets a score of 100
+	// should also check for cheating
+	// compare the top half to a solution set
+	// then just compare the bottom half to the top half once confirmed that the solution was not modified
+	fmt.Println(d)
+	return int(100 * scaleDuration(d))
 }
 
 type model struct {
-	err       error
-	stopwatch stopwatch.Model
+	err error
 }
 
 func (m model) Init() tea.Cmd {
-	return tea.Sequence(m.stopwatch.Init(), m.stopwatch.Stop())
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -62,7 +99,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "e":
-			return m, tea.Sequence(m.stopwatch.Start(), openEditor("test.txt", m))
+			start, err := ntp.Time(NTP_SERVER)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return m, openEditor("test.txt", start)
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
@@ -72,24 +113,18 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, tea.Quit
 		}
 	}
-	// return m, nil
-	var cmd tea.Cmd
-	m.stopwatch, cmd = m.stopwatch.Update(msg)
-	return m, cmd
+	return m, nil
 }
 
 func (m model) View() string {
 	if m.err != nil {
 		return "Error: " + m.err.Error() + "\n"
 	}
-	s := m.stopwatch.View() + "\n"
-	return s + "Press 'e' to play the Vim challenge! Change the content below the demarcation line to look like that which resides above the line.\nPress 'q' to quit.\n"
+	return "Press 'e' to play the Vim challenge! Change the content below the demarcation line to look like that which resides above the line.\nPress 'q' to quit.\n"
 }
 
 func main() {
-	m := model{
-		stopwatch: stopwatch.NewWithInterval(time.Millisecond),
-	}
+	m := model{}
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
